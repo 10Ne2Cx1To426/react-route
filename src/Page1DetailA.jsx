55efadd6a269b0fe17e8f31731018f26968b2c7c
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-17 18:33:12 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>1ページの詳細A</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>1ページの詳細B</h1>
+    </div>
+  );
+};
